311a7289e8c6cf3452ef40354f415e5800033ba5
Habib-Usman <93896875+Habib-Usman@users.noreply.github.com>
2021-12-20 15:13:44 +1030

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import BookSearch from "./Components/SearchBar";
+
+function App() {
+    return (
+        <div className="App">
+            <BookSearch />
+        </div>
+    );
+}
+
+export default App;
